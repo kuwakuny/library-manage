3a489d78802a45d5bc9fc41e2555aa8e9bd4a965
kuwakuny <104746034+kuwakuny@users.noreply.github.com>
2022-07-13 10:02:20 +0900

    2207131000

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Register from './modals/Register';
-import DeleteMulti from './modals/DeleteMulti';
+// import DeleteMulti from './modals/DeleteMulti';
+
 function App() {
   return (
     < div >
       <Register />
-      <DeleteMulti />
+
+      {/* <DeleteMulti /> */}
     </div >
   );
 }
